3b2793b885826479ed88bb6d9cc23fdb5020e543
PrajuMaharjan <prajumaharjan903@gmail.com>
2026-07-14 19:10:11 +0545

    July 14 commit

--- springStart/src/main/java/id/herald/springStart/Controller/SignUpController.java
+++ springStart/src/main/java/id/herald/springStart/Controller/SignUpController.java
@@ -1,25 +1,46 @@
 package id.herald.springStart.Controller;
 
+import id.herald.springStart.Model.UserTable;
+import id.herald.springStart.Repository.UserRepository;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class SignUpController {
+
+    @Autowired
+    private UserRepository uRepo;
+
     @GetMapping("/signupPage")
     public String signup(){
-        return "signupPage.html";
+        return "signupPage";
     }
 
-    @PostMapping("/signPage")
-    public String signupPost(HttpServletRequest request) {
+    @PostMapping("/signupPage")
+    public String signupPost(HttpServletRequest request,Model m) {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
 
         System.out.println("username: " + username);
         System.out.println("password: " + password);
 
-        return null;
+        String hashPassword= DigestUtils.md5DigestAsHex(password.getBytes());
+
+        UserTable uc=new UserTable();
+        uc.setUsername(username);
+        uc.setPassword(hashPassword);
+        
+
+        m.addAttribute("signupSuccess","You have successfully signed up! Login now!");
+
+        if(uRepo.existsByUsernameAndPassword(username,hashPassword)){
+            return "home";
+        }
+            return "signupPage";
     }
 }
--- springStart/src/main/java/id/herald/springStart/Repository/UserRepository.java
+++ springStart/src/main/java/id/herald/springStart/Repository/UserRepository.java
@@ -1,10 +1,10 @@
 package id.herald.springStart.Repository;
 
-import org.apache.catalina.User;
+import id.herald.springStart.Model.UserTable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User,Long> {
-
+public interface UserRepository extends JpaRepository<UserTable,Long> {
+    boolean existsByUsernameAndPassword(String username,String password);
 }
